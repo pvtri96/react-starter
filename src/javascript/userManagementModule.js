f2270eee82bbcfed789da371486e83a3c82ccc27
quangtn <quangtn@bkhightech.vn>
2022-11-07 09:49:45 +0700

    day-1

--- src/javascript/userManagementModule.js
+++ src/javascript/userManagementModule.js
@@ -1,15 +1,27 @@
 export let users = [];
 
-export function createUser(firstName, lastName, age, workAt) {}
+export function createUser(firstName, lastName, age, workAt) {
+  const user = {firstName, lastName, age, workAt};
+  users.push(user);
+}
 
-export function findAllUsers() {}
-export function findUserById(id) {}
-export function findUsers(params) {}
+export function findAllUsers() {
+  return users;
+}
+export function findUserById(id) {
+  return users.filter(user => user.id === id);
+}
+export function findUsers(params) {
+  const { firstName, lastName } = params;
+  return users.filter(user => user.firstName === firstName || user.lastName === lastName)
+}
 
 export function updateUserById() {}
 export function updateUsers(searchParams, payload) {}
 
-export function deleteUserById(id) {}
+export function deleteUserById(id) {
+  users = users.filter(user => user.id !== id);
+}
 export function deleteUsers(searchParams) {}
 
 // For unit tests only
